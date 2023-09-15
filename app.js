201e36a36645960a68b187f24fb682c596c10f73
xPapashi <dtomkowski1@gmail.com>
2023-09-15 17:56:57 +0100

    Modify player input Player can now select grid for their symbol Symbol is added to the array by replacing the one in place.

--- app.js
+++ app.js
@@ -1,30 +1,57 @@
 const gameBoard = (() => {
-  const gBoardArr = ["X", "X", "O", "O", "O", "X", "X", "O", "X"];
-  return { gBoardArr };
-})();
-
-const Player = () => {};
-
-const manageGrid = () => {
-  // const {gBoardArr} = gameBoard;
+  //   const gBoardArr = ["X", "X", "O", "O", "O", "X", "X", "O", "X"];
+  const gBoardArr = ["", "", "", "", "", "", "", "", ""];
   const main = document.querySelector(".main");
   const grid = document.createElement("div");
-  grid.classList.add("grid");
 
-  const createGrid = (gridArr) => {
-    for (let object in gridArr) {
-      const item = document.createElement(`div`);
-      item.classList.add(`item`);
-      item.innerHTML = gridArr[object];
-      grid.appendChild(item);
-    }
-    main.appendChild(grid);
+  const manageGrid = () => {
+    grid.classList.add("grid");
+
+    const createGrid = (gridArr) => {
+      for (let object in gridArr) {
+        const item = document.createElement(`div`);
+        item.classList.add(`item`);
+        item.setAttribute("data-index", object);
+        item.innerHTML = gridArr[object];
+        grid.appendChild(item);
+      }
+      main.appendChild(grid);
+    };
+
+    return { createGrid };
   };
 
-  return { createGrid };
+  const addToArray = (index, value) => {
+    console.log(`Added ${value} to the index of ${index} in array!`);
+    gBoardArr.splice(index, 1, value);
+  };
+
+  return { gBoardArr, manageGrid, addToArray };
+})();
+
+const Player = (status) => {
+  const itemsEventListener = () => {
+    const start = () => {
+      const item = document.querySelectorAll(".item");
+
+      item.forEach((element) => {
+        element.addEventListener("click", () => {
+          const index = element.getAttribute("data-index");
+          element.innerHTML = status;
+          gameBoard.addToArray(index, status);
+        });
+      });
+    };
+    return { start };
+  };
+  return { status, itemsEventListener };
 };
 
-const displayController = (() => {
-  const game = manageGrid();
-  game.createGrid(gameBoard.gBoardArr);
+const game = (() => {
+  //Setup game grid
+  gameBoard.manageGrid().createGrid(gameBoard.gBoardArr);
+
+  //Create Player
+  const player = Player("X");
+  player.itemsEventListener().start();
 })();
