4be2e3afc6ce556f003d559a7351fe14c8c7abb3
xPapashi <dtomkowski1@gmail.com>
2023-09-15 14:19:31 +0100

    Add basic template for cross and nots

---
+++ app.js
@@ -0,0 +1,21 @@
+const gameBoard = (() => {
+    const gBoardArr = ["test", "niggesr", "club21"];
+    const init = () => {};
+    return {gBoardArr, init};
+})();
+
+const Player = () => {
+
+};
+
+function createGrid() {
+    const main = document.querySelector(".main");
+    // const grid = document.createElement('div');
+    // grid.classList.add('grid');
+    
+
+    // main.appendChild(grid);
+}
+
+createGrid();
+console.log(gameBoard.gBoardArr);
