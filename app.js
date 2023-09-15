1515edd706d94b96af4987c5d1ae04ec9d30fe78
xPapashi <dtomkowski1@gmail.com>
2023-09-15 14:35:47 +0100

    Add func to display array in grid

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const gameBoard = (() => {
-    const gBoardArr = ["test", "niggesr", "club21"];
+    const gBoardArr = [
+        "X", "X", "O",
+        "O", "O", "X",
+        "X", "O", "X"
+    ];
     const init = () => {};
     return {gBoardArr, init};
 })();
@@ -8,14 +12,25 @@ const Player = () => {
 
 };
 
-function createGrid() {
+const createGrid = () => {
+    const {gBoardArr} = gameBoard;
     const main = document.querySelector(".main");
-    // const grid = document.createElement('div');
-    // grid.classList.add('grid');
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+
+    for (let object in gBoardArr) {
+        console.log(gBoardArr[object]);
+        const item = document.createElement(`div`);
+        item.classList.add(`item`);
+        item.innerHTML = gBoardArr[object];
+        grid.appendChild(item);
+
+    }
+
+    console.log(gBoardArr);
     
 
-    // main.appendChild(grid);
+    main.appendChild(grid);
 }
 
 createGrid();
-console.log(gameBoard.gBoardArr);
